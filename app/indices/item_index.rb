9655a1154caf6e7c14920a678e9b1e15b113b796
David Henderson <=>
2017-01-09 08:05:57 -0800

    sphinx crap

--- app/indices/item_index.rb
+++ app/indices/item_index.rb
@@ -1,6 +1,6 @@
 ThinkingSphinx::Index.define :item, :with => :active_record do
-  indexes name, :sortable => true
-  indexes mfgname
-  indexes mfgcode
-  indexes ordercode
+  indexes :name, :sortable => true
+  indexes :mfgname
+  indexes :mfgcode
+  indexes :ordercode
 end
